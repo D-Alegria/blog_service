31eb8de7486be79a6278912a60c62ce7e7af7f8a
Demilade <demiladeoladugba@gmail.com>
2020-06-06 14:18:06 +0100

    initial commit

---
+++ src/main/java/com/demtem/blog_service/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demtem.blog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demtem/blog_service/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demtem.blog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
